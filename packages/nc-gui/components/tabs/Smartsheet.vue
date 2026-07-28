f1c68197f9070f8c7f14b1979f3efcf605b722d2
rejectliu <rejectliu@gmail.com>
2026-07-28 16:09:16 +0800

    Revert "✨ feat: support independently wrapped table and view tabs (#4)"
    This reverts commit 9d8edfebc72f310c567c934e5df28450f97e8668.

--- packages/nc-gui/components/tabs/Smartsheet.vue
+++ packages/nc-gui/components/tabs/Smartsheet.vue
@@ -189,7 +189,6 @@ const onDrop = async (event: DragEvent) => {
 }
 
 const { leftSidebarWidth, windowSize, isFullScreen } = storeToRefs(useSidebarStore())
-const isTopbarWrapped = ref(false)
 
 const { isPanelExpanded, extensionPanelSize } = useExtensions()
 
@@ -294,11 +293,10 @@ watch(isViewsLoading, async () => {
   <div
     class="nc-container relative flex flex-col h-full"
     :class="{ 'children:pointer-events-none': isEeUI && showBaseAccessRequestOverlay }"
-    :style="!isFullScreen && isTopbarWrapped ? { '--topbar-height': '5rem' } : undefined"
     @drop="onDrop"
     @dragover.prevent
   >
-    <SmartsheetTopbar v-if="!isFullScreen" @wrap-change="isTopbarWrapped = $event" />
+    <SmartsheetTopbar v-if="!isFullScreen" />
     <div style="height: calc(100% - var(--topbar-height))">
       <NcFullScreen v-if="openedViewsTab === 'view'" v-model="isFullScreen" class="h-full" :page-only="true">
         <!-- Splitpanes is conditionally rendered only after mount to avoid race conditions with its internal async resize logic. -->
